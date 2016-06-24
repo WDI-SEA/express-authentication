0b0a3696c29c34398b2255027472c51da5101cdd
Brian Hague <bhague1281@gmail.com>
2016-06-24 16:34:36 -0700

    fixed test to clear db on auth test

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,6 +1,13 @@
 var expect = require('chai').expect;
 var request = require('supertest');
 var app = require('../index');
+var db = require('../models');
+
+before(function(done) {
+  db.sequelize.sync({ force: true }).then(function() {
+    done();
+  });
+});
 
 describe('Auth Controller', function() {
   describe('GET /auth/signup', function() {
