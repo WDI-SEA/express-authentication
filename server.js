9ded22475e14f71cc8b9795d8ab620ae55836329
009kings <009kings@gmail.com>
2021-01-20 14:30:35 -0800

    SECRET SESSION

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
+const session = require('express-session');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -9,6 +10,12 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.get('/', (req, res) => {
   res.render('index');
