ad6e6dccf30becd6979ac0032ca1381569622636
009kings <009kings@gmail.com>
2021-01-21 11:42:58 -0800

    ugly messages but they show

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -7,6 +7,7 @@ module.exports = (req, res, next) => {
     // }
     if (!req.user) {
         console.log(`🛑 You can't be here`);
+        req.flash('error', 'You must be logged in to access that page')
         res.redirect('/auth/login');
     } else {
         next();
